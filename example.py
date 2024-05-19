d3d19f6adaf03605f8f64aab448bdc444e8c8d43
leo@macbook <leo.damato.dev@gmail.com>
2024-05-19 13:45:04 +0200

    updated render method: now it supports ansi rendering mode

--- example.py
+++ example.py
@@ -1,28 +1,31 @@
 # NOTE: to run this you must install additional dependencies
 
 if __name__=='__main__':
-    import logging, os, sys
-    from gym_simplegrid.envs import SimpleGridEnv
-    from datetime import datetime as dt
+    import os
+    import sys
+    import logging
+    import logging.config
     import gymnasium as gym
+    from datetime import datetime as dt
+    from gym_simplegrid.envs import SimpleGridEnv
     from gymnasium.utils.save_video import save_video
 
     # Folder name for the simulation
     FOLDER_NAME = dt.now().strftime('%Y-%m-%d %H:%M:%S')
     os.makedirs(f"log/{FOLDER_NAME}")
 
-    # Logger to have feedback on the console and on a file
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
         handlers=[logging.StreamHandler(sys.stdout)]
     )
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger()
 
     logger.info("-------------START-------------")
 
     options ={
         'start_loc': 12,
+        'goal_loc': (2,0)
         # goal_loc is not specified, so it will be randomly sampled
     }
 
@@ -44,25 +47,25 @@ if __name__=='__main__':
     done = env.unwrapped.done
 
     logger.info("Running action-perception loop...")
-    
-    with open(f"log/{FOLDER_NAME}/history.csv", 'w') as f:
-        f.write(f"step,x,y,reward,done,action\n")
         
-        for t in range(500):
-            #img = env.render(caption=f"t:{t}, rew:{rew}, pos:{obs}")
-            
-            action = env.action_space.sample()
-            f.write(f"{t},{info['agent_xy'][0]},{info['agent_xy'][1]},{rew},{done},{action}\n")
-            
-            if done:
-                logger.info(f"...agent is done at time step {t}")
-                break
-            
-            obs, rew, done, _, info = env.step(action)
+    for t in range(500):
+        action = env.action_space.sample()
+        if done:
+            logger.info(f"...agent is done at time step {t}")
+            break
+        obs, rew, done, _, info = env.step(action)
             
     if env.render_mode == 'rgb_array_list':
         frames = env.render()
         save_video(frames, f"log/{FOLDER_NAME}", fps=env.fps)
+    
+    if env.render_mode == 'ansi_list':
+        frames = env.render()
+        with open(f"log/{FOLDER_NAME}/history.csv", 'w') as f:
+            f.write(f"step,x,y,reward,done,action\n")
+            for frame in frames:
+                f.write(frame)
+
     logger.info("...done")
     logger.info("-------------END-------------")
     
